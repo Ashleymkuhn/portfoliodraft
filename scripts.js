9af90473408ead6bf24edabf1ed65ca1ab46412d
Ashleymkuhn <108952441+Ashleymkuhn@users.noreply.github.com>
2023-01-19 14:22:53 -0800

    responsive

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function myFunction() {
   const element = document.getElementById("titles");
   element.scrollIntoView();
-}
+};
+
+$(document).ready(function(){
+  $('.burgermenu').on('click', function(){
+      $('.mob-nav').toggle();
+  })
+});
+
 
 
 
